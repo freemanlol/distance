83717c182778b9b9f175a1e27272852f6be7dba2
Freeman <48633960+freemanlol@users.noreply.github.com>
2020-06-05 19:34:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const menuBtn = document.querySelector(".m-menu");
+const menu = document.querySelector('.menu-sidebar');
+
+menuBtn.addEventListener('click', function () {
+  menu.classList.toggle("active");
+});
+
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    return false;
+  }
+}
